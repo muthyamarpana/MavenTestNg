b1062d504e74c52764775eb6ac1c67f3397d9fc7
ArpanaM <muriki.arpana@gmail.com>
2025-07-24 22:17:07 +0530

    New class added in a new branch

---
+++ src/main/java/testSce/Test12.java
@@ -0,0 +1,10 @@
+package testSce;
+
+public class Test12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("New Branch is created...");
+	}
+
+}
